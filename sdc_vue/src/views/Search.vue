e7b64e1d36d492bb053ec13caed2c6139c8a16f2
Smith <JASMITH@CONTRACTOR.USGS.GOV>
2019-12-06 14:18:20 -0500

    Added postman collection and got some of the filters working on the search page.

--- sdc_vue/src/views/Search.vue
+++ sdc_vue/src/views/Search.vue
@@ -2,28 +2,63 @@
   <div id="searchPageContainer" class="container">
     <h2>Search SDC</h2>
     <reactive-base
-      app="good-books-yj"
-      credentials="gBgUqs2tV:3456f3bf-ea9e-4ebc-9c93-08eb13e5c87c"
+      app="sdc_items"
+      credentials="6m5d8utyq0:oa9qjxi1lu"
+      url="https://sdc-testing-396737610.us-east-1.bonsaisearch.net"
     >
-      <div class="filters-container">
-        <multi-list
-          componentId="Authors"
-          dataField="authors.raw"
-          class="filter"
-          title="Select Authors"
-          selectAllLabel="All Authors"
-        />
-        <single-range
-          componentId="Ratings"
-          dataField="average_rating"
-          :data="[
-            { start: 0, end: 3, label: 'Rating < 3' },
-            { start: 3, end: 4, label: 'Rating 3 to 4' },
-            { start: 4, end: 5, label: 'Rating > 4' }
-          ]"
-          title="Book Ratings"
-          class="filter"
-        />
+      <div class="row">
+        <div class="filters-container col-md-5">
+          <multi-list
+            componentId="Keyword"
+            dataField="metadata.resourceInfo.keyword.keyword.keyword.keyword"
+            title="Keywords"
+            :URLParams="true"
+            :react="{
+              and: ['Keyword', 'Author']
+            }"
+          />
+          <br />
+          <br />
+          <multi-list
+            componentId="Author"
+            dataField="contact.name.keyword"
+            title="Authors"
+            :URLParams="true"
+            :react="{
+              and: ['Keyword', 'Author']
+            }"
+          />
+        </div>
+
+        <div class="results-container col-md-5">
+          Text Search
+          <data-search
+            componentId="Text Search"
+            :dataField="[
+                  'metadata.resourceInfo.citation.title',
+                  'metadata.resourceInfo.abstract'
+                ]"
+            :autosuggest="true"
+            :showFilter="true"
+            :URLParams="true"
+            placeholder="Search for text in title or abstract"
+          />
+          <br />
+          <br />
+          <reactive-list
+            componentId="SearchResult"
+            dataField="metadata.resourceInfo.citation.title.keyword"
+            paginationAt="both"
+            :react="{
+              and: ['Keyword', 'Text Search', 'Author']
+            }"
+          >
+          <div slot="renderItem" slot-scope="{ res }">
+            {{res.metadata.resourceInfo.citation.title}}
+          </div>
+          </reactive-list>
+
+        </div>
       </div>
     </reactive-base>
     <br />
@@ -33,10 +68,10 @@
     <br />
     <br />
     <br />
-    <br />
-    <br />
   </div>
 </template>
+
+<style scoped></style>
 
 <script>
 export default {
